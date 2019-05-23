c1b3924b4cd9194045c18af1f8c551db505d0019
pauchino09 <49151129+pauchino09@users.noreply.github.com>
2019-05-23 13:36:14 +0200

    Arranque BD Oracle

---
+++ oraclestart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ORACLE_HOME=/home/alumno/oracle-install-18c
+ORACLE_SID=asir
+PATH=$PATH:$ORACLE_HOME/bin/
+
+export ORACLE_HOME
+export ORACLE_SID
+
+lsnrctl start
+
+sqlplus / as sysdba <<EOF
+startup open
+EOF
+
+lsnrctl status
