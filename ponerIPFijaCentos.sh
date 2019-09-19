5f5bc74ead679bee5824a4df84acdccc07255391
chunche95 <paeste95.pb@gmail.com>
2019-09-19 14:09:17 +0200

    Script de configuracion iniciales de Oracle

---
+++ ponerIPFijaCentos.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Poner IP fija en Centos 7
+
+sudo nano -c /etc/sysconfig/network-scripts/ifcfg-enp0s3 <<EOF
+BOOTPROTO=static
+ONBOOT=yes
+IPADDR=10.1.0.95
+GATEWAY=10.1.0.1
+DNS1=8.8.8.8
+DNS2=8.8.4.4
+EOF
