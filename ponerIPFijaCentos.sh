b72738d162e63b99118c08565ee225edecba7a15
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-09-25 10:57:30 +0200

    Asignar IP fija a Centos 7

--- ponerIPFijaCentos.sh
+++ ponerIPFijaCentos.sh
@@ -5,12 +5,14 @@
 sudo nano -c /etc/sysconfig/network-scripts/ifcfg-enp0s3 <<EOF
 BOOTPROTO=static
 ONBOOT=yes
-IPADDR=10.1.0.95
+IPADDR=10.1.0.51
+NETMASK=255.255.0.0
 GATEWAY=10.1.0.1
+NETWORK=10.1.0.0
 DNS1=8.8.8.8
 DNS2=8.8.4.4
 EOF
 
-systemctl stop NetworkManager
-systemctl disable NetworkManager
-systemctl restart network.service
+sudo systemctl stop NetworkManager
+sudo systemctl disable NetworkManager
+sudo systemctl restart network.service
