71a4c7898e005b6592cf0ebb1b61b325222637f8
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-17 22:50:02 +0200

    Update crearUsuarios.sh

--- practica/crearUsuarios.sh
+++ practica/crearUsuarios.sh
@@ -3,7 +3,8 @@
 for i in `cat lista`;
 do 
   echo "create user $i identified by $i quota 10M on paraotros;>>crear.sql
-  echo "user and password ===> OK!"
+  echo "create user BERMUDEZ identified by BERMUDEZ51 quota unlimited on Users;>>crear.sql
+    echo "user and password ===> OK!"
   echo "grant connect, resource to $i;">>crear.sql
   echo "Privileges ===> OK!"
  done
