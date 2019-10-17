5032cc54f761d05b1c952775bba0c06a9572ff12
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-17 22:50:05 +0200

    Acceso a otros usuarios
    Creacion de sinonimos de mis tablas, creacion de roles para la lectura,

---
+++ practica/darAccesoAOtrosUsuarios.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sqlpus / as sysdba<<EOF
+
+create public synonym escuderias for BERMUDEZ.escuderias
+create public synonym pilotos for BERMUDEZ.pilotos
+create public synonym prototipos for BERMUDEZ.prototipos
+create public synonym carreras for BERMUDEZ.carreras
+create public synonym resultados for BERMUDEZ.resultados
+create public synonym circuitos for BERMUDEZ.circuitos
+
+create role LECTURATABLAS
+grant select on escuderias to LECTURATABLAS;
+grant select on pilotos to LECTURATABLAS;
+grant select on prototipos to LECTURATABLAS;
+grant select on carreras to LECTURATABLAS;
+grant select on resultados to LECTURATABLAS;
+grant select on circuitos to LECTURATABLAS;
+
+EOF
+
+for i in `cat lista`;
+do 
+  echo "grant LECTURATABLAS to $i;">>/home/alumno/privilegiosOtrosUsuarios.sql
+  echo "Script terminado"
+done
+
+# Lanzo el script 'privilegiosOtrosUsuarios.sql' de SQL con sys en SQLPLUS
+sqlpus -s BERMUDEZ/BERMUDEZ51@10.1.35.51/asir as sysdba @/home/alumno/privilegiosOtrosUsuarios.sql
