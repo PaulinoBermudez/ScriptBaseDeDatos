457bf6879be686ce1a07957dea0e2d23a5e66169
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-17 19:55:27 +0200

    crear tablespace PARAOTROS.dbf

---
+++ practica/crearTablespace.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Accedemos al directorio donde crear el directorio 
+cd /home/alumno  
+# Creamos el directorio del tablespace  
+mkdir datos                   
+# Creamos el tablespace en sqlplus  
+sqlplus / as sysdba<<EOF           
+create tablespace paraotros       
+datafile '/home/alumno/datos/paraotros.dbf'    
+size 800k                                 
+autoextend on                             
+next 200k                                    
+maxsize 100M;                                
+exit                               
+EOF                                                                                    
