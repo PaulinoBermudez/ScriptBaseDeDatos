1545686d8e43f74abcb42f3e4089654a8cd5eaf4
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-21 13:41:54 +0200

    ej4. Creación de las tablas - tablespace CARRERAS.
    Creación de tablespace CARRERAS con un datafile en /datos/carreras.
    Con BERMUDEZ lanzo el script carreras.sql modificado -> carreras tablespace

--- practica/tablespaceCarreras.sh
+++ practica/tablespaceCarreras.sh
@@ -3,7 +3,7 @@
 cd /home/alumno
 # Creamos el directorio del tablespace
 mkdir datos/carreras  
-# Creamos el tablespace en sqlplus  
+# Creamos el tablespace en sqlplus con SYS.
 sqlplus / as sysdba<<EOF 
 create tablespace carreras  
 datafile '/home/alumno/datos/carreras/carreras.dbf'  
@@ -13,3 +13,6 @@ next 200k
 maxsize 100M;   
 exit      
 EOF 
+# Lanzamos el script de carreras de SQL con BERMUDEZ Modificado para que almacene las tablas en el TABLESPACE CARRERAS.
+sqlplus BERMUDEZ/BERMUDEZ51@10.1.35.51/asir @/home/alumno/carreras-coches.sql
+exit
