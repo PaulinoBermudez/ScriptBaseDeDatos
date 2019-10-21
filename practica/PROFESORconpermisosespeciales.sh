0a9b17c87ce4196397138ab4466361b18c138768
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-21 13:36:00 +0200

    PROFESOR con permisos de lectura en las V.sistema
    PROFESOR con permisos de lectura en las V.sistema DBA_TABLES & DBA_TABLESPACES.

--- practica/PROFESORconpermisosespeciales.sh
+++ practica/PROFESORconpermisosespeciales.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# PROFESOR con permisos de lectura en las V.sistema DBA_TABLES & DBA_TABLESPACES.
+# Conectamos con sys. pero damos los privilegios sobre la tabla de BERMUDEZ. 
+
 sqlplus / as sysdba<<EOF
 GRANT SELECT ON BERMUDEZ.DBATABLES TO PROFESOR;
 GRANT SELECT ON BERMUDEZ.DBATABLESPACES TO PROFESOR;
