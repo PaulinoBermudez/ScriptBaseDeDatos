4e747c7cfe732e19a696dae2ae2f24aeffb0cbbe
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-21 13:50:23 +0200

    EJ5. Dar acceso a otros usuarios. Part.1
    Dar acceso a otros usuarios a un campo de mis tablas.

--- practica/darAccesoAOtrosUsuarios.sh
+++ practica/darAccesoAOtrosUsuarios.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
+# Creacion de sinónimos, del rol para asignar los sinónimos.
 sqlpus / as sysdba<<EOF
 
-create public synonym escuderias for BERMUDEZ.escuderias
-create public synonym pilotos for BERMUDEZ.pilotos
-create public synonym prototipos for BERMUDEZ.prototipos
-create public synonym carreras for BERMUDEZ.carreras
-create public synonym resultados for BERMUDEZ.resultados
-create public synonym circuitos for BERMUDEZ.circuitos
+create public synonym escuderias for BERMUDEZ.escuderias;
+create public synonym pilotos for BERMUDEZ.pilotos;
+create public synonym prototipos for BERMUDEZ.prototipos;
+create public synonym carreras for BERMUDEZ.carreras;
+create public synonym resultados for BERMUDEZ.resultados;
+create public synonym circuitos for BERMUDEZ.circuitos;
 
-create role LECTURATABLAS
-grant select on escuderias to LECTURATABLAS;
-grant select on pilotos to LECTURATABLAS;
-grant select on prototipos to LECTURATABLAS;
-grant select on carreras to LECTURATABLAS;
-grant select on resultados to LECTURATABLAS;
-grant select on circuitos to LECTURATABLAS;
+create role LECTURATABLAS;
+
+grant select on BERMUDEZ.escuderias to LECTURATABLAS;
+grant select on BERMUDEZ.pilotos to LECTURATABLAS;
+grant select on BERMUDEZ.prototipos to LECTURATABLAS;
+grant select on BERMUDEZ.carreras to LECTURATABLAS;
+grant select on BERMUDEZ.resultados to LECTURATABLAS;
+grant select on BERMUDEZ.circuitos to LECTURATABLAS;
 
 EOF
 
+# script para dar a todos los usuarios el privilegio de LECTURATABLAS.
 for i in `cat lista`;
 do 
-  echo "grant LECTURATABLAS to $i;">>/home/alumno/privilegiosOtrosUsuarios.sql
+  echo "grant LECTURATABLAS to $i;">>privilegiosOtrosUsuarios.sql
   echo "Script terminado"
 done
 
-# Lanzo el script 'privilegiosOtrosUsuarios.sql' de SQL con sys en SQLPLUS
-./sqlpus -s BERMUDEZ/BERMUDEZ51@10.1.35.51/asir @/home/alumno/privilegiosOtrosUsuarios.sql
+# Lanzo el script 'privilegiosOtrosUsuarios.sql' de SQL con sys en SQLPLUS, es el único con capacidad para lanzar y asignar el role.
+sqlpus / as sysdba @privilegiosOtrosUsuarios.sql
