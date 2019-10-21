318fa0a582cfc9bf91ccb3eb53a9f38b062856e3
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-21 13:24:37 +0200

    EJ2.Crear usuarios para otros usuarios.
    Crear usuario  para el resto de usuarios, para ello se lee una lista de usuarios del fichero 'lista', se les otorga privilegios en el tablespace PARAOTROS de 10Mb. Para mi usuario asigno el tablespace users.

--- practica/crearUsuarios.sh
+++ practica/crearUsuarios.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
+# El resto de usuarios.
 for i in `cat lista`;
 do 
-  echo "create user $i identified by $i quota 10M on paraotros;>>crear.sql
-  echo "create user BERMUDEZ identified by BERMUDEZ51 quota unlimited on Users;>>crear.sql
-    echo "user and password ===> OK!"
+  echo "create user $i identified by $i quota 10M on paraotros;">>crear.sql
+  echo "user and password ===> OK!"
   echo "grant connect, resource to $i;">>crear.sql
   echo "Privileges ===> OK!"
- done
- 
+done
+# Mi usuario BERMUDEZ
+echo "create user BERMUDEZ identified by BERMUDEZ51 quota unlimited on Users;">>crear.sql
+echo "user and password ===> OK!"
+echo "grant connect, resource to BERMUDEZ;">>crear.sql
+echo "Privileges ===> OK!"
