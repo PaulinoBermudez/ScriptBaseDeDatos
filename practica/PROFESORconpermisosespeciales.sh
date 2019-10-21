aa03b96bdbaefd08984186f34d26871ddfca8462
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-21 13:36:43 +0200

    EJ3.PROFESOR con permisos de lectura especiales
    PROFESOR con permisos de lectura en las V.sistema DBA_TABLES & DBA_TABLESPACES.

--- practica/PROFESORconpermisosespeciales.sh
+++ practica/PROFESORconpermisosespeciales.sh
@@ -2,7 +2,6 @@
 
 # PROFESOR con permisos de lectura en las V.sistema DBA_TABLES & DBA_TABLESPACES.
 # Conectamos con sys. pero damos los privilegios sobre la tabla de BERMUDEZ. 
-
 sqlplus / as sysdba<<EOF
 GRANT SELECT ON BERMUDEZ.DBATABLES TO PROFESOR;
 GRANT SELECT ON BERMUDEZ.DBATABLESPACES TO PROFESOR;
