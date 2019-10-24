db55e76df748eb31f4f9ec57d2045c6e1a93a640
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-24 13:52:54 +0200

    EJ3. PROFESOR con permisos especiales

--- practica/PROFESORconpermisosespeciales.sh
+++ practica/PROFESORconpermisosespeciales.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# PROFESOR con permisos de lectura en las V.sistema DBA_TABLES & DBA_TABLESPACES.
-# Conectamos con sys. pero damos los privilegios sobre la tabla de BERMUDEZ. 
+###################################################################################
+# Ejercicio 3 :PROFESOR con permisos especiales.                                  #
+# Haz que el usuario PROFESOR tenga permisos de lectura en las vistas de sistema  #
+# DBATABLES y DBATABLESPACES.                                                     #
+###################################################################################
+# PROFESOR con permisos de lectura en las V.sistema DBA_TABLES & DBA_TABLESPACES. #
+# Conectamos con sys. para dar los privilegios.                                   #
+###################################################################################
+
 sqlplus / as sysdba<<EOF
 drop role permisosProfe;
 create role permisosProfe;
