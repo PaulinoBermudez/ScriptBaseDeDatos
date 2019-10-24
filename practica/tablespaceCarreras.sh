e61a4c6f99892d9d6b1fb54472f0100b94aee890
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-24 14:01:20 +0200

    EJ4. CREACION DE LAS TABLAS

--- practica/tablespaceCarreras.sh
+++ practica/tablespaceCarreras.sh
@@ -1,46 +1,32 @@
 #!/bin/sh  
+
+############################################################################################################
+# Ejercicio 4 : Creacion de las tablas.                                                                    #
+#                                                                                                          #
+# Cambia la contraseña de tu usuario, si no lo has hecho ya, para que ningun compañero pueda utilizarlo    #
+# (conALTER USER).                                                                                         #
+# 1. Crea un tablespacede nombre CARRERAS, con un datafile en el directorio /datos/carreras.               #
+# 2. Con tu propio usuario, crea las tablas en ese tablespace.                                             #
+#  2.1 Utiliza el script carreras-coches.sql para la creación de las tablas.                               #
+#  2.2 Tendrás que modificar elscript para que tenga en cuenta el nuevo tablespace                         #
+############################################################################################################
+
 # Accedemos al directorio donde crear el directorio
 cd /home/alumno
 # Creamos el directorio del tablespace
 mkdir datos/carreras  
 # Creamos el tablespace en sqlplus con SYS.
 sqlplus / as sysdba<<EOF 
+drop tablespace carreras;
 create tablespace carreras  
 datafile '/home/alumno/datos/carreras/carreras.dbf'  
 size 800k                
 autoextend on                  
 next 200k 
 maxsize 100M;   
-
 alter user BERMUDEZ default tablespace carreras quota on unlimited;
-
 exit      
-<<<<<<< HEAD
-EOF
-=======#!/bin/sh  
-# Accedemos al directorio donde crear el directorio
-cd /home/alumno
-# Creamos el directorio del tablespace
-mkdir datos/carreras  
-# Creamos el tablespace en sqlplus con SYS.
-sqlplus / as sysdba<<EOF 
-create tablespace carreras  
-datafile '/home/alumno/datos/carreras/carreras.dbf'  
-size 800k                
-autoextend on                  
-next 200k 
-maxsize 100M;   
-
-alter user BERMUDEZ default tablespace carreras quota on unlimited;
-
-exit      
-<<<<<<< HEAD
-EOF
-=======
-EOF 
-
 EOF 
 # Lanzamos el script de carreras de SQL con BERMUDEZ Modificado para que almacene las tablas en el TABLESPACE CARRERAS.
-sqlplus BERMUDEZ/BERMUDEZ51@10.1.35.51/asir @/home/alumno/carreras-coches.sql
+sqlplus BERMUDEZ/BERMUDEZ51@10.1.35.51/asir @carreras-coches.sql
 exit
->>>>>>> 62b494015636bd3bde21f146e1108b30ab1129ef
