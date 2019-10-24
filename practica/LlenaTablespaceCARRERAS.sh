a4a8ce5dff12efbb0c0c8ef521817679d505b454
chunche95 <37621958+chunche95@users.noreply.github.com>
2019-10-24 17:52:53 +0200

    EJ.6 LLENA EL TABLESPACE CARRERAS

--- practica/LlenaTablespaceCARRERAS.sh
+++ practica/LlenaTablespaceCARRERAS.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+#################################################################################################
+# Ejercicio 6 : Llena eltablespace CARRERAS.                                                    #
+# Llena de datos el tablespace CARRERAS                                                         #
+#    - Indica el metodo que utilizas para llenarlo de datos y qué mensaje de error aparece      #
+#    - Amplıa el tablespace con un nuevo datafile para se puedan insertar mas datos.            #
+#################################################################################################
+
 # Llenamos el tablespace CARRERAS con un bucle. Nos conectamos con el USER: BERMUDEZ.
 sqlplus BERMUDEZ/BERMUDEZ51@10.1.35.51/asir<<EOF
-
+drop table TABLALLENA;
 create table TABLALLENA VALORES(VARCHAR(255)) on tablespace carreras;
-
 begin
-for i in 1 .. 10000 loop
+for i in 1 .. 100000 loop
 insert into TABLALLENA values($i);
 loop end;
 end;
@@ -21,7 +27,6 @@ size 2M
 autoextend on
 next 200k
 maxsize 10M;
-
 alter user BERMUDEZ quota 5M on carreras;
 exit
 EOF
