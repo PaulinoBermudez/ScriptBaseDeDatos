ff50a041f74602a26c9b4dd02a8363648c7f18ea
chunche95 <paeste95.pb@gmail.com>
2019-11-04 21:11:13 +0100

    Script de Base de Datos Oracle en Centos 7

--- gitHub.sh
+++ gitHub.sh
@@ -6,13 +6,29 @@ echo "+--------------------------+"
 echo "| Añadiendo archivos a git |"
 echo "+--------------------------+"
 sleep 2
-
+git add Apuntes/. &&
+git commit -m "Apuntes de Base de Datos 2" &&
+git add Script_introducción_18-19/. && 
+git push origin master 
+echo "**************"
+echo "Apuntes ==> OK"
+echo "**************"
+git commit -m "Script iniciales de bases de datos en Centos 7" &&
+echo "**************"
+echo "Scripts ==> OK"
+echo "**************"
+git push origin master
 git add . &&
 git commit -m "Script de Base de Datos Oracle en Centos 7" &&
 git push origin master
+echo "**************"
+echo "Practica ==> OK"
+echo "**************"
 sleep 5
 clear
 git status
+echo "" 
+echo "" 
 echo "+--------------------------+"
 echo "|     Trabajo terminado    |"
 echo "+--------------------------+"
