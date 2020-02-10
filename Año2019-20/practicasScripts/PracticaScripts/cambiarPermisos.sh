d3bd732cf258e0127001381affbef0819b1387f0
chunche95 <paeste95.pb@gmail.com>
2020-02-10 15:20:16 -0500

    Solucion en el zip

--- Año2019-20/practicasScripts/PracticaScripts/cambiarPermisos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo cd /etc/systemd/system/
-
-sudo systemctl stop oracle.service
-sudo systemctl start oracle.service
-sudo systemctl enable oracle.service
-sudo systemctl daemon-reload
