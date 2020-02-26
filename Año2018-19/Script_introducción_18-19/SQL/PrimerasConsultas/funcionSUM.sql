d2b9f3869c8f33385b105fce6a11acc6857a8cd2
chunche95 <paeste95.pb@gmail.com>
2020-02-26 22:11:15 +0100

    Otros archivos - Subidos

---
+++ Año2018-19/Script_introducción_18-19/SQL/PrimerasConsultas/funcionSUM.sql
@@ -0,0 +1,8 @@
+select sum(salary) from employees;
+
+/* Sumamos 2 por cada empleado */
+select sum(2) from employees;
+
+select sum(distinct salary) from employees;
+
+select sum(commission_pct) from employees;
